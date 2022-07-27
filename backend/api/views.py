dc5a04a938e2b8e1786455912c8de11f27cb18bd
mojtabangh <mojtaba83ngh@gmail.com>
2022-07-27 20:58:07 +0430

    jalase 24 - filter 3

--- backend/api/views.py
+++ backend/api/views.py
@@ -25,6 +25,14 @@ class ArticleViewSet(ModelViewSet):
     queryset = Article.objects.all()
     serializer_class = ArticleSerializer
     filterset_fields = ['status', 'author__username']
+    search_fields = [
+        'title',
+        'content',
+        'author__username',
+        'author__first_name',
+        'author__last_name',
+        ]
+
 
     def get_permissions(self):
         if self.action in ['list', 'create']:
