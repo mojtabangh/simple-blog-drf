6e4675a97375e654946562059848d2b6441870c2
mojtabangh <mojtaba83ngh@gmail.com>
2022-08-13 20:59:18 +0430

    jalase 28 - Nested Serializers

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -3,12 +3,24 @@ from rest_framework import serializers
 
 from blog.models import Article
 
+class AuthorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = get_user_model()
+        fields = ['id', 'username', 'first_name', 'last_name']
+
+
 class ArticleSerializer(serializers.ModelSerializer):
+    author = AuthorSerializer()
     class Meta:
         model = Article
         fields = ('__all__')
         # exclude = ('created', 'updated')
 
+        def validate_title(self, value):
+            if 'django' not in value.lower():
+                raise serializers.ValidationError("Blog post is not about Django")
+            return value
+           
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
